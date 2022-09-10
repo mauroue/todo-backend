f7c76e34246928ab874af25ded0150267d0cf363
Mauro Ue <mauroue@gmail.com>
2022-09-10 19:38:59 -0300

    removed secret

--- tasks_app/settings.py
+++ tasks_app/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u!l91f!8-77*agd%xuu+v3s$j%ye-b(s70fca%#duy(f25vnen'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
