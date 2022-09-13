5554b04ae7eebd4ba2da343ebb3267ec2f30b81c
Mauro Ue <mauroue@gmail.com>
2022-09-13 10:03:24 -0300

    changed variable name and set debug to false

--- tasks/models.py
+++ tasks/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
     
 
     def __str__(self):
-        return self.title
+        return self.text
